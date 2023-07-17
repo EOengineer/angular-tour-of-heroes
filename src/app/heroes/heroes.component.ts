650faf5f01a95555d49146c4a3254b25f2306329
Eric Oligney <eoengineer@gmail.com>
2023-07-17 13:18:40 -0400

    adds hero service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,24 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent {
+  constructor(private heroService: HeroService) {}
+
   hero: Hero = {
     id: 1,
     name: 'Windstorm'
   }
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
   
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
